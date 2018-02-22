a4864fdd85f019bf516ed9b49d0813290445b256
Andrew van Rooyen <andrew@nomanini.com>
2018-02-22 14:44:10 +0200

    yarn run clean

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -2,41 +2,23 @@
  * HomePage
  *
  * This is the first thing users see of our App, at the '/' route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
 
-import injectReducer from 'utils/injectReducer';
-import injectSaga from 'utils/injectSaga';
-import reducer from './reducer';
-import saga from './saga';
-
-export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div>
-        Hello
-      </div>
+      <h1>
+        <FormattedMessage {...messages.header} />
+      </h1>
     );
   }
 }
-
-export function mapDispatchToProps(dispatch) {
-  return {};
-}
-
-const mapStateToProps = createStructuredSelector({});
-
-const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
-const withReducer = injectReducer({ key: 'home', reducer });
-const withSaga = injectSaga({ key: 'home', saga });
-
-export default compose(
-  withReducer,
-  withSaga,
-  withConnect,
-)(HomePage);
